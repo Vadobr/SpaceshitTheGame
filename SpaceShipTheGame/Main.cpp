413ebe74ce5d1f043eb217e4a5fbd6f09bf13f1b
SilverMilver03 <svadya2000@ukr.net>
2023-01-30 13:56:33 +0200

    Project started

---
+++ SpaceShipTheGame/Main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#include <conio.h>
+
+int main() {
+
+	std::cout << "This is Spaceship";
+
+    std::cout << " " << std::endl;
+        std::cout << "------!" << std::endl;
+        std::cout << "------!" << std::endl;
+        std::cout << "------^" << std::endl;
+        std::cout << "-----/ \\" << std::endl;
+        std::cout << "----/ _ \\" << std::endl;
+        std::cout << "----|= =|" << std::endl;
+        std::cout << "---|     |" << std::endl;
+        std::cout << "---|     |" << std::endl;
+        std::cout << "-|-|     | |" << std::endl;
+        std::cout << "-|-|     | |" << std::endl;
+        std::cout << "-|-|     | |" << std::endl;
+        std::cout << "---|     |" << std::endl;
+        std::cout << "---|     |" << std::endl;
+        std::cout << "---|     |" << std::endl;
+        std::cout << "---|     |" << std::endl;
+        std::cout << "--/|##!##|\\" << std::endl;
+        std::cout << "-/| ##!## |\\" << std::endl;
+        std::cout << "/ | ##!## | \\" << std::endl;
+        std::cout << "|  /^ | ^\\  |" << std::endl;
+        std::cout << "| /  (|)  \\ |" << std::endl;
+        std::cout << "|/   (|)   \\|" << std::endl;
+        std::cout << "-----(())" << std::endl;
+        std::cout << "----((:))" << std::endl;
+        std::cout << "----((:))" << std::endl;
+        std::cout << "-----(())" << std::endl;
+        std::cout << "----(())" << std::endl;
+        std::cout << "-----()" << std::endl;
+        std::cout << "-----." << std::endl;
+        std::cout << "------." << std::endl;
+        std::cout << "-----." << std::endl;
+        
+    std::cout << std::endl << std::endl;
+
+    std::cout << "Wooo woo\n\n";
+
+    _getch();
+
+	return 0;
+
+}
